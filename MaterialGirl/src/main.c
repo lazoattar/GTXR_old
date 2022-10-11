fd5da5229b09ae37a87658804b65c9f3732fca69
Lazo Attar <lazo@overlord.attlocal.net>
2022-10-11 11:32:06 -0400

    UART TX with DMA

--- MaterialGirl/src/main.c
+++ MaterialGirl/src/main.c
@@ -1,79 +1,91 @@
 #define STM32F767xx
 
+#include <stdio.h>
 #include <stdint.h>
 #include "stm32f7xx.h"
 
 #define STACK_TOP   ((uint32_t)0x20000800)
 #define AF07        (0x7UL)
+#define CH04        (0x4UL)
 
 static void NMI_Handler(void);
 static void HardFault_Handler(void);
 static void Default_Handler(void);
-static void USART3_Write(unsigned char x);
-static unsigned char USART3_Read(void);
-static int USART3_Available(void);
+static void USART3_DMA1_Stream3_Write(char *data, uint16_t length);
+static void DMA1_Stream3_IRQHandler(void);
+static void Delay(int ms);
 int main(void);
 
+uint8_t tx_finished;
+char uart_tx_data[40];
+uint8_t counter = 0;
+uint8_t length;
+
 // Vector Table
-uint32_t *myvectors[52] __attribute__ ((section("vectors"))) = {
-    (uint32_t *) STACK_TOP,             // Stack pointer
-    (uint32_t *) main,                  // Code entry point
-    (uint32_t *) NMI_Handler,           // NMI Handler
-    (uint32_t *) HardFault_Handler,     // Hard fault handler
-    (uint32_t *) Default_Handler,       // Memory management
-    (uint32_t *) Default_Handler,       // Pre-fetch fault, memory access fault
-    (uint32_t *) Default_Handler,       // Undefined instruction or illegal state
-    (uint32_t *) Default_Handler,       // Reserved
-    (uint32_t *) Default_Handler,       // Reserved
-    (uint32_t *) Default_Handler,       // Reserved
-    (uint32_t *) Default_Handler,       // Reserved
-    (uint32_t *) Default_Handler,       // System service call via SWI instruction
-    (uint32_t *) Default_Handler,       // Debug Monitor
-    (uint32_t *) Default_Handler,       // Reserved
-    (uint32_t *) Default_Handler,       // Pendable request for system service
-    (uint32_t *) Default_Handler,       // System tick timer
-    (uint32_t *) Default_Handler,       // Window Watchdog interrupt
-    (uint32_t *) Default_Handler,       // PVD through EXTI line detection interrupt
-    (uint32_t *) Default_Handler,       // Tamper and TimeStamp interrupts through the EXTI line
-    (uint32_t *) Default_Handler,       // RTC Wakeup interrupt through the EXTI line
-    (uint32_t *) Default_Handler,       // Flash global interrupt
-    (uint32_t *) Default_Handler,       // RCC global interrupt
-    (uint32_t *) Default_Handler,       // EXTI Line0 interrupt
-    (uint32_t *) Default_Handler,       // EXTI Line1 interrupt
-    (uint32_t *) Default_Handler,       // EXTI Line2 interrupt
-    (uint32_t *) Default_Handler,       // EXTI Line3 interrupt
-    (uint32_t *) Default_Handler,       // EXTI Line4 interrupt
-    (uint32_t *) Default_Handler,       // DMA1 Stream0 global interrupt
-    (uint32_t *) Default_Handler,       // DMA1 Stream1 global interrupt
-    (uint32_t *) Default_Handler,       // DMA1 Stream2 global interrupt
-    (uint32_t *) Default_Handler,       // DMA1 Stream3 global interrupt
-    (uint32_t *) Default_Handler,       // DMA1 Stream4 global interrupt
-    (uint32_t *) Default_Handler,       // DMA1 Stream5 global interrupt
-    (uint32_t *) Default_Handler,       // DMA1 Stream6 global interrupt
-    (uint32_t *) Default_Handler,       // ADC1, ADC2 and ADC3 global interrupts
-    (uint32_t *) Default_Handler,       // CAN1 TX interrupts
-    (uint32_t *) Default_Handler,       // CAN1 RX0 interrupts
-    (uint32_t *) Default_Handler,       // CAN1 RX1 interrupt
-    (uint32_t *) Default_Handler,       // CAN1 SCE interrupt
-    (uint32_t *) Default_Handler,       // EXTI Line[9:5] interrupts
-    (uint32_t *) Default_Handler,       // TIM1 Break interrupt and TIM9 global interrupt
-    (uint32_t *) Default_Handler,       // TIM1 Update interrupt and TIM10 global interrupt
-    (uint32_t *) Default_Handler,       // TIM1 Trigger and Commutation interrupts and TIM11 global interrupt
-    (uint32_t *) Default_Handler,       // TIM1 Capture Compare interrupt
-    (uint32_t *) Default_Handler,       // TIM2 global interrupt
-    (uint32_t *) Default_Handler,       // TIM3 global interrupt
-    (uint32_t *) Default_Handler,       // TIM4 global interrupt
-    (uint32_t *) Default_Handler,       // I2C1 event interrupt
-    (uint32_t *) Default_Handler,       // I2C1 error interrupt
-    (uint32_t *) Default_Handler,       // I2C2 event interrupt
-    (uint32_t *) Default_Handler,       // I2C2 error interrupt
-    (uint32_t *) Default_Handler        // SPI1 global interrupt
+uint32_t *myvectors[56] __attribute__ ((section("vectors"))) = {
+    (uint32_t *) STACK_TOP,                 // Stack pointer
+    (uint32_t *) main,                      // Code entry point
+    (uint32_t *) NMI_Handler,               // NMI Handler
+    (uint32_t *) HardFault_Handler,         // Hard fault handler
+    (uint32_t *) Default_Handler,           // Memory management
+    (uint32_t *) Default_Handler,           // Pre-fetch fault, memory access fault
+    (uint32_t *) Default_Handler,           // Undefined instruction or illegal state
+    (uint32_t *) Default_Handler,           // Reserved
+    (uint32_t *) Default_Handler,           // Reserved
+    (uint32_t *) Default_Handler,           // Reserved
+    (uint32_t *) Default_Handler,           // Reserved
+    (uint32_t *) Default_Handler,           // System service call via SWI instruction
+    (uint32_t *) Default_Handler,           // Debug Monitor
+    (uint32_t *) Default_Handler,           // Reserved
+    (uint32_t *) Default_Handler,           // Pendable request for system service
+    (uint32_t *) Default_Handler,           // System tick timer
+    (uint32_t *) Default_Handler,           // Window Watchdog interrupt
+    (uint32_t *) Default_Handler,           // PVD through EXTI line detection interrupt
+    (uint32_t *) Default_Handler,           // Tamper and TimeStamp interrupts through the EXTI line
+    (uint32_t *) Default_Handler,           // RTC Wakeup interrupt through the EXTI line
+    (uint32_t *) Default_Handler,           // Flash global interrupt
+    (uint32_t *) Default_Handler,           // RCC global interrupt
+    (uint32_t *) Default_Handler,           // EXTI Line0 interrupt
+    (uint32_t *) Default_Handler,           // EXTI Line1 interrupt
+    (uint32_t *) Default_Handler,           // EXTI Line2 interrupt
+    (uint32_t *) Default_Handler,           // EXTI Line3 interrupt
+    (uint32_t *) Default_Handler,           // EXTI Line4 interrupt
+    (uint32_t *) Default_Handler,           // DMA1 Stream0 global interrupt
+    (uint32_t *) Default_Handler,           // DMA1 Stream1 global interrupt
+    (uint32_t *) Default_Handler,           // DMA1 Stream2 global interrupt
+    (uint32_t *) DMA1_Stream3_IRQHandler,   // DMA1 Stream3 global interrupt
+    (uint32_t *) Default_Handler,           // DMA1 Stream4 global interrupt
+    (uint32_t *) Default_Handler,           // DMA1 Stream5 global interrupt
+    (uint32_t *) Default_Handler,           // DMA1 Stream6 global interrupt
+    (uint32_t *) Default_Handler,           // ADC1, ADC2 and ADC3 global interrupts
+    (uint32_t *) Default_Handler,           // CAN1 TX interrupts
+    (uint32_t *) Default_Handler,           // CAN1 RX0 interrupts
+    (uint32_t *) Default_Handler,           // CAN1 RX1 interrupt
+    (uint32_t *) Default_Handler,           // CAN1 SCE interrupt
+    (uint32_t *) Default_Handler,           // EXTI Line[9:5] interrupts
+    (uint32_t *) Default_Handler,           // TIM1 Break interrupt and TIM9 global interrupt
+    (uint32_t *) Default_Handler,           // TIM1 Update interrupt and TIM10 global interrupt
+    (uint32_t *) Default_Handler,           // TIM1 Trigger and Commutation interrupts and TIM11 global interrupt
+    (uint32_t *) Default_Handler,           // TIM1 Capture Compare interrupt
+    (uint32_t *) Default_Handler,           // TIM2 global interrupt
+    (uint32_t *) Default_Handler,           // TIM3 global interrupt
+    (uint32_t *) Default_Handler,           // TIM4 global interrupt
+    (uint32_t *) Default_Handler,           // I2C1 event interrupt
+    (uint32_t *) Default_Handler,           // I2C1 error interrupt
+    (uint32_t *) Default_Handler,           // I2C2 event interrupt
+    (uint32_t *) Default_Handler,           // I2C2 error interrupt
+    (uint32_t *) Default_Handler,           // SPI1 global interrupt
+    (uint32_t *) Default_Handler,           // SPI2 global interrupt
+    (uint32_t *) Default_Handler,           // USART1 global interrupt
+    (uint32_t *) Default_Handler,           // USART2 global interrupt
+    (uint32_t *) Default_Handler            // USART3 global interrupt
 };
 
 int main(void)
 {
-    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;           // enable GPIOD clock
-    RCC->APB1ENR |= RCC_APB1ENR_USART3EN;          // enable USART3 clock
+    RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN;            // enable GPIOD clock
+    RCC->APB1ENR |= RCC_APB1ENR_USART3EN;           // enable USART3 clock
+    RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;             // enable DMA1 clock
 
     GPIOD->MODER |= GPIO_MODER_MODER8_1;
     GPIOD->MODER |= GPIO_MODER_MODER9_1;
@@ -83,14 +95,28 @@ int main(void)
 
     USART3->BRR = 0x008B;
     USART3->CR1 = 0;
-    USART3->CR1 |= (USART_CR1_TE | USART_CR1_RE | USART_CR1_UE);
+    USART3->CR3 |= USART_CR3_DMAT;
+    USART3->CR1 |= (USART_CR1_TE | USART_CR1_UE);
 
+
+    DMA1_Stream3->CR &=~(DMA_SxCR_EN);
+	while((DMA1_Stream3->CR &(DMA_SxCR_EN)));
+
+    DMA1_Stream3->CR |= ((CH04 << 25)
+                        |(DMA_SxCR_MINC)
+                        |(DMA_SxCR_DIR_0)
+                        |(DMA_SxCR_TCIE));
+
+    DMA1_Stream3->PAR = (uint32_t) &USART3->TDR;
+
+    NVIC_EnableIRQ(DMA1_Stream3_IRQn);
+
+    char *hi = "hi\r\n";
     while(1)
     {
-        if (USART3_Available())
-        {
-            USART3_Write(USART3_Read());
-        }
+
+		USART3_DMA1_Stream3_Write(hi,4);
+        Delay(1000);
     }
 }
 
@@ -109,27 +135,35 @@ void Default_Handler(void)
     for(;;);
 }
 
-void USART3_Write(unsigned char x)
+void USART3_DMA1_Stream3_Write(char *data, uint16_t length)
 {
-    USART3->TDR = (x);
-    while(!((USART3->ISR) & USART_ISR_TC)){;}
+    DMA1_Stream3->M0AR = data;
+    DMA1_Stream3->NDTR = length;
+    DMA1_Stream3->CR |= DMA_SxCR_EN;
+    while(tx_finished == 0);
+    tx_finished = 0;
+
 }
 
-unsigned char USART3_Read(void)
+void DMA1_Stream3_IRQHandler(void)
 {
-    return USART3->RDR;
+	if(DMA1->LISR & DMA_LISR_TCIF3)
+	{
+		tx_finished = 1;
+		DMA1->LIFCR = DMA_LIFCR_CTCIF3;
+	}
 }
 
-int USART3_Available(void)
+void Delay(int ms)
 {
-    if ((USART3->ISR) & USART_ISR_RXNE)
-    {
-        return 1;
-    }
-
-    else
-    {
-        return 0;
-    }
+	int i;
+	SysTick->LOAD = 16000 - 1;
+	SysTick->VAL = 0;
+	SysTick->CTRL = 0x5;
+	for (i = 0; i < ms; i++)
+	{
+		while(!(SysTick->CTRL & 0x10000)){}
+	}
+	SysTick->CTRL = 0;
 }
 
