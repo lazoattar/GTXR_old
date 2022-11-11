0f4eb825212f3a907405c28f212ea8617f9287af
vmalepati1 <malepativikas@gmail.com>
2022-11-10 21:03:29 -0500

    Implement log level functionality

--- MaterialGirl/src/util/log.c
+++ MaterialGirl/src/util/log.c
@@ -8,7 +8,16 @@ extern void USART3_DMA1_Stream3_Write(char *data, uint16_t length);
 
 void log_message(int level, const char *format, bool newline, ...) {
     int len = strlen(format);
-    int len_padded = len * 2 + 1;
+    // Add 2 for (potential) newline and null character
+    // Add 11 for log level
+     
+    // Log levels:
+    // [FATAL]: 
+    // [ERROR]:
+    // [WARNING]: 
+    // [INFO]: 
+
+    int len_padded = len * 2 + 2 + 11;
 
     char buffer[len_padded];
 
@@ -18,6 +27,29 @@ void log_message(int level, const char *format, bool newline, ...) {
     va_start(args, format);
     write_len = sprintf(buffer, format, args);
     va_end(args);
+
+    if (newline == true) {
+        buffer[write_len] = '\n';
+        write_len++;
+        buffer[write_len] = '\0';
+    }
+
+    switch (level) {
+        case LOG_LEVEL_FATAL:
+            sprintf(buffer, "%s%s", "[FATAL]: ", buffer);
+            break;
+        case LOG_LEVEL_ERROR:
+            sprintf(buffer, "%s%s", "[ERROR]: ", buffer);
+            break;
+        case LOG_LEVEL_WARN:
+            sprintf(buffer, "%s%s", "[WARNING]: ", buffer);
+            break;
+        case LOG_LEVEL_INFO:
+            sprintf(buffer, "%s%s", "[INFO]: ", buffer);
+            break;
+        default:
+            break;
+    }
 
     USART3_DMA1_Stream3_Write(buffer, write_len);
 }
