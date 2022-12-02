f4abea9235e6b40c7344a797c7e489e6ceee4259
vmalepati1 <malepativikas@gmail.com>
2022-12-02 00:00:37 -0500

    Fix log function

--- MaterialGirl/src/util/log.c
+++ MaterialGirl/src/util/log.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <stdarg.h>
 
 extern void USART3_DMA1_Stream3_Write(char *data, uint16_t length);
 
-void log_message(int level, const char *format, bool newline, ...) {
+void log_message(int level, bool newline, const char *format, ...) {
     int len = strlen(format);
     // Add 2 for (potential) newline and null character
     // Add 11 for log level
@@ -17,39 +18,40 @@ void log_message(int level, const char *format, bool newline, ...) {
     // [WARNING]: 
     // [INFO]: 
 
-    int len_padded = len * 2 + 2 + 11;
+    int len_padded = len * 5 + 2 + 11;
 
     char buffer[len_padded];
-
     int write_len = 0;
-
+    
     va_list args;
+
+    switch (level) {
+        case LOG_LEVEL_FATAL:
+            write_len = sprintf(buffer, "%s", "[FATAL]: ");
+            break;
+        case LOG_LEVEL_ERROR:
+            write_len = sprintf(buffer, "%s", "[ERROR]: ");
+            break;
+        case LOG_LEVEL_WARN:
+            write_len = sprintf(buffer, "%s", "[WARNING]: ");
+            break;
+        case LOG_LEVEL_INFO:
+            write_len = sprintf(buffer, "%s", "[INFO]: ");
+            break;
+        default:
+            break;
+    }
+    
     va_start(args, format);
-    write_len = sprintf(buffer, format, args);
-    va_end(args);
+    write_len += vsprintf(buffer + write_len, format, args);
 
     if (newline == true) {
         buffer[write_len] = '\n';
         write_len++;
         buffer[write_len] = '\0';
     }
-
-    switch (level) {
-        case LOG_LEVEL_FATAL:
-            sprintf(buffer, "%s%s", "[FATAL]: ", buffer);
-            break;
-        case LOG_LEVEL_ERROR:
-            sprintf(buffer, "%s%s", "[ERROR]: ", buffer);
-            break;
-        case LOG_LEVEL_WARN:
-            sprintf(buffer, "%s%s", "[WARNING]: ", buffer);
-            break;
-        case LOG_LEVEL_INFO:
-            sprintf(buffer, "%s%s", "[INFO]: ", buffer);
-            break;
-        default:
-            break;
-    }
-
+    
     USART3_DMA1_Stream3_Write(buffer, write_len);
+    
+    va_end(args);
 }
--- MaterialGirl/src/util/log.h
+++ MaterialGirl/src/util/log.h
@@ -12,35 +12,35 @@
 #define LOG_LEVEL LOG_LEVEL_INFO
 #endif /* LOG_LEVEL */
 
-void log_message(int level, const char *format, bool newline, ...);
+void log_message(int level, bool newline, const char *format, ...);
 
 #if LOG_LEVEL >= LOG_LEVEL_FATAL
-#define LOG_FATAL(format, ...) log_message(LOG_LEVEL_FATAL, format, true, ##__VA_ARGS__)
-#define _LOG_FATAL(format, ...) log_message(LOG_LEVEL_FATAL, format, false, ##__VA_ARGS__)
+#define LOG_FATAL(format, ...) log_message(LOG_LEVEL_FATAL, true, format, ##__VA_ARGS__)
+#define _LOG_FATAL(format, ...) log_message(LOG_LEVEL_FATAL, false, format, ##__VA_ARGS__)
 #else
 #define LOG_FATAL(format, ...)
 #define _LOG_FATAL(format, ...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_ERROR
-#define LOG_ERROR(format, ...) log_message(LOG_LEVEL_ERROR, format, true, ##__VA_ARGS__)
-#define _LOG_ERROR(format, ...) log_message(LOG_LEVEL_ERROR,  format, false, ##__VA_ARGS__)
+#define LOG_ERROR(format, ...) log_message(LOG_LEVEL_ERROR, true, format, ##__VA_ARGS__)
+#define _LOG_ERROR(format, ...) log_message(LOG_LEVEL_ERROR, false, format, ##__VA_ARGS__)
 #else
 #define LOG_ERROR(format, ...)
 #define _LOG_ERROR(format, ...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_WARN
-#define LOG_WARN(format, ...) log_message(LOG_LEVEL_WARN, format, true, ##__VA_ARGS__)
-#define _LOG_WARN(format, ...) log_message(LOG_LEVEL_WARN, format, false, ##__VA_ARGS__)
+#define LOG_WARN(format, ...) log_message(LOG_LEVEL_WARN, true, format, ##__VA_ARGS__)
+#define _LOG_WARN(format, ...) log_message(LOG_LEVEL_WARN, false, format, ##__VA_ARGS__)
 #else
 #define LOG_WARN(format, ...)
 #define _LOG_WARN(format, ...)
 #endif
 
 #if LOG_LEVEL >= LOG_LEVEL_INFO
-#define LOG_INFO(format, ...) log_message(LOG_LEVEL_INFO, format, true, ##__VA_ARGS__)
-#define _LOG_INFO(format, ...) log_message(LOG_LEVEL_INFO, format, false, ##__VA_ARGS__)
+#define LOG_INFO(format, ...) log_message(LOG_LEVEL_INFO, true, format, ##__VA_ARGS__)
+#define _LOG_INFO(format, ...) log_message(LOG_LEVEL_INFO, false, format, ##__VA_ARGS__)
 #else
 #define LOG_INFO(format, ...)
 #define _LOG_INFO(format, ...)
